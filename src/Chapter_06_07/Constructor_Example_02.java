d95e1b1a5db7fe9958c194607770236ff8afad93
ChoYoungSang <ChoYoungSang@choys>
2022-01-20 18:39:58 +0900

    배포자 :조영상 배포내용 : 생성자 추가.

--- src/Chapter_06_07/Constructor_Example_02.java
+++ src/Chapter_06_07/Constructor_Example_02.java
@@ -4,7 +4,7 @@ public class Constructor_Example_02 {
 
 	public static void main(String[] args) {
 
-		Constructor_Example_01 myCar = new Constructor_Example_01("검정", 3000);
+		Constructor_Example_01 myCar1 = new Constructor_Example_01("검정", 3000);
 		
 		// Constructor_Example_01 myCar = new Constructor_Example_01(); (X) // 기본생성자는 호출할 수 없다.
 		
